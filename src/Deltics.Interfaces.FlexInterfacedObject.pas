8ac6d7c41ce77686ece6648bf457e6cad5a7f9af
Jolyon Direnko-Smith <jsmith@deltics.co.nz>
2020-12-20 09:43:25 +1300

    enhance: Added RefCountIsDisabled property (readonly)

--- src/Deltics.Interfaces.FlexInterfacedObject.pas
+++ src/Deltics.Interfaces.FlexInterfacedObject.pas
@@ -24,6 +24,7 @@ interface
       public
         procedure DisableRefCount;
         property RefCount: Integer read fRefCount;
+        property RefCountDisabled: Boolean read fRefCountDisabled;
 
       // IUnknown
       protected
